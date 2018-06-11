7a7ebd3d1ce2687ddd9432ed89e1b9d876e89356
DaniRubin <37410225+DaniRubin@users.noreply.github.com>
2018-06-12 01:42:59 +0300

    Add files via upload
    First working project

---
+++ masWork.py
@@ -0,0 +1,195 @@
+tdLine = "<td>"
+travelLine='<td colspan=1>'
+
+
+
+def	copyFiles(fileHtml,file):
+	f = open(fileHtml,'r',encoding='utf-8')
+	file = open(file,'w')
+	lines = f.readlines()
+
+	for line in lines:
+		file.write(line)
+
+	file.close()
+
+def cancelAllTheEnd(file,endFile):
+	f = open(file,'r')
+	badLineNum=0
+	lines = f.readlines()
+	for line in lines:
+		badLineNum +=1
+		if endFile in line:
+				break	
+	f.seek(0)
+
+	lines = f.readlines()
+	f.close()
+	f = open(file,'w')
+	num=0
+	for line in lines:
+		num+=1
+		if num<=badLineNum:
+			f.write(line)
+	f.close()
+
+	
+
+def getNumOfHours(line):
+	hours=0 
+	minutes=0
+	if len(line)==11:
+		line = line[6:]
+		hours=(int)(line[0])
+		minutes=(int)(line[2:4])
+		
+	if len(line)==12:
+		line = line[6:]
+		hours=(int)(line[0:2])
+		minutes=(int)(line[3:5])
+
+	hours+=(minutes/60)
+	#print(line,hours)
+	return hours
+
+
+
+def findHowMuchTime(file,NumOfLine):
+	#file= open(file,"r")
+	f = open(file,'r')
+	num=0
+	
+	for line in f.readlines():
+		num+=1
+		if NumOfLine+8==num  :
+			if not tdLine in line:
+				hours=getNumOfHours(line)
+				break
+			else:
+				num-=1
+	return hours
+	
+
+def calculateMas(key,pay,file,keyToPrint):
+	f = open(file,'r')
+	num=0
+	sum=0
+	for line in f.readlines():
+		num+=1
+		if key in line:
+			time = findHowMuchTime(file,num)
+			sum+=time
+
+	if sum:
+		print(sum*pay," - ףסכ ",sum," - ןמז ", keyToPrint)
+
+
+
+
+	return sum*pay
+
+
+def claculateTravelTotal(line):
+	num=[]
+	for x in line:
+		if x>='0' and x<='9':
+			num.append((int)(x))
+	sum=0
+	if(len(num)==5):
+		sum+=num[0]*10000
+		sum+=num[1]*1000
+		sum+=num[2]*100
+		sum+=num[3]*10
+		sum+=num[4]*1
+
+	if(len(num)==4):
+		sum+=num[0]*1000
+		sum+=num[1]*100
+		sum+=num[2]*10
+		sum+=num[3]*1
+
+
+	if(len(num)==3):
+		sum+=num[0]*100
+		sum+=num[1]*10
+		sum+=num[2]*1
+	
+	if(len(num)==2):
+		sum+=num[0]*10
+		sum+=num[1]*1
+	
+
+	return sum
+
+
+
+
+
+
+
+def claculateTravelMoney(key,file,keyToPrint):
+	f = open(file,"r")
+	num=0
+	money=0
+	seen = False
+	check=False
+	for line in f.readlines():
+		num+=1
+		if check:
+			money = claculateTravelTotal(line)
+			break
+		if travelLine in line:
+			if not seen:
+				seen = True
+			else:
+				check=True
+	print(money," - ףסכ ", keyToPrint)
+
+	return money
+
+
+def main():
+	theValues= {("הכרדה",'>הדרכה<'):70,
+				(" HTML חותיפ",'פיתוח תוכן וירטואלי HTML (מוקד לאומי)'):70,
+				("ןכות חותיפ",'>פיתוח תוכן<'):70,
+				("ב.ש תקידב",'בדיקת ש.ב'):50,
+				("ןמז לוטיב",'ביטול זמן'):28.5,
+				("לגס תרשכה",'>הכשרת סגל<'):30,
+				("הפלחה הכרדה",'הדרכה - החלפה'):70,
+				("תונויואר",'ראיונות מועמדים-שיווק\מיון\ראיונות'):50,
+				("תעד תווח",'סקרים\שאלונים\חוות דעת'):35,
+				("המאתה תדעו",'השתתפות בועדת התאמה'):50,
+				("זלסקיפ",'הדרכה - פיקסלז'):50,
+				("לגס תרשכה זלסקיפ","הכשרת סגל - פיקסלז"):35,
+				("איש םוי", 'יום פעילות-יום שיא'):50,
+				("םיסקט יוול",'ליווי טקסים\כנסים'):50,
+				("ןחבמ תקידב","בדיקת מבחן"):50,
+				("סרוק תקידב","בקרה על תוכן (QA) ומשוב"):50,
+				("ילאוטוריו הכרדה","הדרכה בקורס וירטואלי"):70,
+				("פייקס","תגבור וירטואלי - בסקייפ"):50,
+				("ץיק הנחמ","מחנה קיץ"):50
+				}
+
+	travel="נסיעות מיוחדות"
+	endFile = '<!-- end: PAGE CONTENT-->'
+	file= r'C:\Users\user1\Desktop\mascoret\lol.txt'
+	fileHtml= r'C:\Users\user1\Desktop\mascoret\CyberNet_ monthly_reports.html'
+
+	copyFiles(fileHtml,file)
+
+	cancelAllTheEnd(file,endFile)
+
+
+	sum=0
+	for Allkey , pay in theValues.items():
+		keyToPrint,key=Allkey
+		sum+= calculateMas(key,pay,file,keyToPrint)	
+	sum+= claculateTravelMoney(travel,file,"תועיסנ")
+		
+	print("\n\n\nAll the amount is - ",sum)
+
+
+
+
+
+main()
